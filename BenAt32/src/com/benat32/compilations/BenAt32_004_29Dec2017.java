7b32f55f8ea5ed46da562f184f8b363470b36906
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2017-12-30 00:19:26 +0800

    day 4 - enum

---
+++ BenAt32/src/com/benat32/compilations/BenAt32_004_29Dec2017.java
@@ -0,0 +1,42 @@
+package com.benat32.compilations;
+
+public class BenAt32_004_29Dec2017 {
+
+	enum Size {
+		SMALL,
+		MEDIUM,
+		LARGE
+	}
+	
+	private enum Flavor {
+		CHOCOLATE(100),
+		VANILLA(120),
+		STRAWBERRY(80);
+		
+		private int fCalories;
+		
+		void setCalaries(int aCalories){
+			fCalories = aCalories;
+		}
+		
+		private Flavor(int aCalories){
+			fCalories = aCalories;
+		}
+		
+		int getCalories (){
+			return fCalories;
+		}
+	}
+	
+	public static void main(String[] args){
+		System.out.println("My size: " + Size.MEDIUM);
+		System.out.println("Chocolate calories: " + Flavor.CHOCOLATE.getCalories());
+		Flavor.CHOCOLATE.setCalaries(150);
+		System.out.println("Chocolate calories: " + Flavor.CHOCOLATE.getCalories());
+		
+	}
+	
+	private static void log(Object aMessage){
+		System.out.println(String.valueOf(aMessage));
+	}	
+}
