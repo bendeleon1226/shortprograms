19896f3b9eece9b58eb8ef43970620cf6a012985
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-05 00:00:24 +0800

    day 10 - using find, start, and end methods of Matcher class

---
+++ BenAt32/src/com/benat32/compilations/BenAt32_010_04Jan2018.java
@@ -0,0 +1,26 @@
+package com.benat32.compilations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BenAt32_010_04Jan2018 {
+
+	public static void main(String[] args) {
+		String text = "This is the text which is to be searched " +
+				"for occurrences of the word 'is'.";
+		
+		String patternString = "is";
+		
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(text);
+		
+		int count = 0;
+		while(matcher.find()){
+			count++;
+			System.out.println("found: " + count + " : "
+					+ matcher.start() + " - " + matcher.end());
+			System.out.println(text.substring(matcher.start(),matcher.end()));
+		}
+	}
+
+}
