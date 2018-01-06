03e5be66f7349d3a990b6a0831e7a862055a96f6
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-06 23:23:23 +0800

    day 11 - using Matcher.group function

---
+++ BenAt32/src/com/benat32/compilations/BenAt32_011_05Jan2018.java
@@ -0,0 +1,20 @@
+package com.benat32.compilations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BenAt32_011_05Jan2018 {
+	public static void main(String[] args){
+		String text = "John writes about this, and John writes about that," +
+				" and John writes about everything. ";
+		String patternString = "(John)";
+		
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()){
+			System.out.println("found at group 0:" + matcher.group(0));
+			System.out.println("found at group 1:" + matcher.group(1));
+		}
+	}
+}
