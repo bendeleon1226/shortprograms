e0a7265938645d0ba3eb2caa9b1b86f758033c6a
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-09 21:44:56 +0800

    day 13 - group within group

---
+++ BenAt32/src/com/benat32/compilations/BenAt32_013_07Jan2018.java
@@ -0,0 +1,22 @@
+package com.benat32.compilations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BenAt32_013_07Jan2018 {
+	public static void main(String[] args){
+		String text = "John writes about this, and John writes about that," +
+				" and John writes about everything.";
+		String patternString = "((John) (.+?) )";
+		
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()){
+			System.out.println("found at group 0:" + matcher.group(0));
+			System.out.println("found at group 1:" + matcher.group(1));
+			System.out.println("found at group 2:" + matcher.group(2));
+			System.out.println("found at group 3:" + matcher.group(3));
+		}
+	}
+}
