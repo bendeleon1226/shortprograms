d4ba8c93f851416420359a27922c2b6d9cfca658
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-09 21:49:06 +0800

    java object to be used for converting Java object to xml

---
+++ BenAt32/src/com/benat32/beans/Book.java
@@ -0,0 +1,27 @@
+package com.benat32.beans;
+
+public class Book {
+	private String title;
+	private String author;
+	private String type;
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
